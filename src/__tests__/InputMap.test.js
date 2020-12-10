fb048a48a955e9ca68b4cef8981d3c10d7df16df
Wesley Clements <wesley.clements@gmail.com>
2020-12-09 16:07:07 -0800

    returned interal test

--- src/__tests__/InputMap.test.js
+++ src/__tests__/InputMap.test.js
@@ -4,11 +4,4 @@ describe("InputMap", () => {
   it("should be constructable", () => {
     expect(new InputMap()).toBeInstanceOf(InputMap);
   });
-  it("should have internal methods", () => {
-    const map = new InputMap();
-    expect(map).toHaveProperty("__internal");
-    expect(map.__internal).toHaveProperty("getActions");
-    expect(map.__internal).toHaveProperty("keyHasAction");
-    expect(map.__internal).toHaveProperty("buttonHasAction");
-  });
 });
