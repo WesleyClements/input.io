84b89002da732f7d39fb26582f9aeb9cce09dfe5
Wesley Clements <wesley.clements@gmail.com>
2020-12-14 10:06:23 -0800

    added util to get current time in milliseconds

--- src/util/index.js
+++ src/util/index.js
@@ -1,10 +1,4 @@
-/**
- * Checks if a value is an array for strings
- * @param {*} value - value to check type of
- * @returns {Boolean}
- */
-export const isStringArray = (value) =>
-  Array.isArray(value) && value.every((str) => typeof str === "string");
+export const getNow = () => (performance ?? Date).now();
 
 export { default as Keys } from "./Keys";
 export { default as MouseButtons } from "./MouseButtons";
